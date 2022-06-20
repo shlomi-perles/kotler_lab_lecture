f45d5508e88739e0088b6f2bce7aeb013f86b9c1
shlomi-perles <shlomip100@gmail.com>
2022-06-20 21:11:02 +0300

    Page fix

--- kotler_lab_lecture/code/tools/fix_manim_editor.py
+++ kotler_lab_lecture/code/tools/fix_manim_editor.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 MAIN_PATH = Path(__file__).resolve().parent.parent.parent.parent
 from collections import OrderedDict
 import json
@@ -6,20 +7,25 @@ import json
 PROJECT_NAME = "docs"
 JSON_FILE = (MAIN_PATH / PROJECT_NAME / "project.json")
 
+
 def sort_sections(dict):
     counter = 0
     last_idx = 0
     last_section_str = ""
     last_section = None
     for section in dict:
-        section["sections_len"] = len(section)
+        section["sections_len"] = len(section["sections"])
+        first_sub_section = section["sections"][0]
         for idx, sub_section in enumerate(section["sections"]):
-            if idx ==0:
+            if sub_section != first_sub_section:
+                sub_section["is_sub_section"] = True
+                sub_section["parent_id"] = sub_section["parent_id"]
                 last_section_str = Path(sub_section["original_video"]).stem[:-5]
             sub_section["in_project_id"] = counter
+            sub_section["id"] = idx
             counter += 1
         last_section = section
-        last_idx = idx
+        parent_counter = counter
 
 
 def main():
@@ -32,6 +38,6 @@ def main():
     with open(JSON_FILE, 'w') as file:
         file.write(json.dumps(sections_dict, indent=4))
 
+
 if __name__ == "__main__":
     main()
-
